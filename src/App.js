e0f8874e06665c6ad9b56697fc491bb3f93d6f3e
Rale <rajkovic.miodrag@gmail.com>
2020-03-21 12:51:04 +0100

    Setup base routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import Home from './components/layout/Home';
+import Signup from './components/auth/Signup';
+import Signin from './components/auth/Signin';
+import ItemsList from './components/items/ItemsList';
+import Item from './components/items/Item';
+import Profile from './components/profile/Profile';
+import Cart from './components/cart/Cart';
 import './App.css';
 
 function App() {
 	return (
-		<div className="App">
-			<h1>Hello</h1>
-		</div>
+		<Router>
+			<div className="App">
+				<Header />
+				<Switch>
+					<Route path="/marketplace/:itemId">
+						<Item />
+					</Route>
+					<Route path="/marketplace">
+						<ItemsList />
+					</Route>
+					<Route path="/cart">
+						<Cart />
+					</Route>
+					<Route path="/profile">
+						<Profile />
+					</Route>
+					<Route path="/signup">
+						<Signup />
+					</Route>
+					<Route path="/signin">
+						<Signin />
+					</Route>
+					<Route path="/">
+						<Home />
+					</Route>
+				</Switch>
+				<Footer />
+			</div>
+		</Router>
 	);
 }
 
