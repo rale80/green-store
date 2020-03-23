667704084e222fcf6946bae38637c378f2bd24bc
Rale <rajkovic.miodrag@gmail.com>
2020-03-23 22:04:21 +0100

    Create Signin component

--- src/components/auth/Signin.js
+++ src/components/auth/Signin.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Form, FormGroup, Input } from 'reactstrap';
+import { MdLockOutline } from 'react-icons/md';
 
 const Signin = props => {
 	return (
 		<div className="Signin">
-			<h1>Signin</h1>
+			<div className="row">
+				<div className="text-center mt-3 mx-auto col-md-6">
+					<MdLockOutline size={42} color="#69bf1e" />
+					<h1 className="font-weight-normal">SignIn</h1>
+					<Form>
+						<FormGroup>
+							<Input
+								className="py-4"
+								type="email"
+								name="email"
+								placeholder="Email"
+								required
+							/>
+						</FormGroup>
+						<FormGroup>
+							<Input
+								className="py-4"
+								type="password"
+								name="password"
+								placeholder="Password"
+								required
+							/>
+						</FormGroup>
+						<Button className="btn-block py-2">Submit</Button>
+						<Link className="float-right mt-2" to="/signup">
+							Already have an account? Sign up
+						</Link>
+					</Form>
+				</div>
+			</div>
 		</div>
 	);
 };
