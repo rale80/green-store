f51042094bb05873c5258b1428c1770de1ccb6f2
Rale <rajkovic.miodrag@gmail.com>
2020-03-23 16:29:48 +0100

    Create Signup component

--- src/components/auth/Signup.js
+++ src/components/auth/Signup.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Form, FormGroup, Input } from 'reactstrap';
+import { MdLockOutline } from 'react-icons/md';
 
 const Signup = props => {
 	return (
-		<div className="Signup">
-			<h1>Signup</h1>
+		<div className="row">
+			<div className="text-center mt-3 mx-auto col-md-6">
+				<MdLockOutline size={42} color="#69bf1e" />
+				<h1 className="font-weight-normal">SignUp</h1>
+				<Form>
+					<FormGroup>
+						<Input
+							className="py-4"
+							type="name"
+							name="name"
+							placeholder="Name"
+							required
+						/>
+					</FormGroup>
+					<FormGroup>
+						<Input
+							className="py-4"
+							type="address"
+							name="address"
+							placeholder="Address"
+							required
+						/>
+					</FormGroup>
+					<FormGroup>
+						<Input
+							className="py-4"
+							type="tel"
+							name="phone"
+							placeholder="Phone"
+							required
+						/>
+					</FormGroup>
+					<FormGroup>
+						<Input
+							className="py-4"
+							type="email"
+							name="email"
+							placeholder="Email"
+							required
+						/>
+					</FormGroup>
+					<FormGroup>
+						<Input
+							className="py-4"
+							type="password"
+							name="password"
+							placeholder="Password"
+							required
+						/>
+					</FormGroup>
+					<Button className="btn-block py-2">Submit</Button>
+					<Link className="float-right mt-2" to="/signin">
+						Already have an account? Sign in
+					</Link>
+				</Form>
+			</div>
 		</div>
 	);
 };
